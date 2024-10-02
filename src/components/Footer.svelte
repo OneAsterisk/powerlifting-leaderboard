d48d13f58c567c515f0c823764741b805ca722ad
OneAsterisk <90720963+OneAsterisk@users.noreply.github.com>
2024-10-01 23:45:31 -0400

    Add Footer component to user interface

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<footer class="footer">
+	<p>&copy Created by Ben Papp. Est. 2024</p>
+</footer>
+
+<style>
+	.footer {
+		background-color: #212529;
+		display: flex;
+		text-align: center;
+		width: 100%;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+	}
+	.footer p {
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 0;
+	}
+</style>
